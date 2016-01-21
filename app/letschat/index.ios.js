cfc272b980653271411bf1b76f2ef351648e3761
hapjs <mailhap@qq.com>
2016-01-21 21:19:39 +0800

    修改标题

--- app/letschat/index.ios.js
+++ app/letschat/index.ios.js
@@ -16,7 +16,7 @@ class letschat extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Let's Chat!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
